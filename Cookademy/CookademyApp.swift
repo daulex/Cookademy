3bac22368271dca8a53f0b75a190ff43125de49e
Kirill Galenko <kirill.galenko@awave.com>
2024-09-17 14:06:12 +0300

    Initial Commit

---
+++ Cookademy/CookademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookademyApp.swift
+//  Cookademy
+//
+//  Created by kg on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
